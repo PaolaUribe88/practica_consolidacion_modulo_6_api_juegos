f204588d1711a7b7c8e8d8ff3147c25494dbf4fc
PaolaUribe88 <paolauribe88@gmail.com>
2023-07-03 22:49:41 -0400

    Primer commit repositorio local

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === "production" ? "/practica_consolidacion_modulo_6_api_juegos_produccion/" : "/",
-//   };
+ module.exports = {
+     publicPath: process.env.NODE_ENV === "production" ? "/practica_consolidacion_modulo_6_api_juegos_produccion/" : "/",
+   };
